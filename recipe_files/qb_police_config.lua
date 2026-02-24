fc5036b550ede1caa7d67d065b6f03e61526ce78
Vincx12 <dylanboullard@gmail.com>
2026-02-24 22:49:21 +0100

    Delete recipe_files directory

--- recipe_files/qb_police_config.lua
+++
@@ -1,8 +0,0 @@
-Config = Config or {}
-Config.Locations = Config.Locations or {}
-Config.Fines = Config.Fines or {
-  ["Excès de vitesse"] = 250,
-  ["Conduite dangereuse"] = 650,
-  ["Feu rouge"] = 200,
-  ["Pas de ceinture"] = 150,
-}
--- recipe_files/qb_vehicleshop_config.lua
+++
@@ -1,8 +0,0 @@
-Config = Config or {}
-Config.Locations = Config.Locations or {}
-Config.Commission = 0.10
-Config.FinanceCommission = 0.05
-Config.PaymentWarning = 10
-Config.PaymentInterval = 30
-Config.MinimumDown = 15
-Config.MaximumPayments = 48
