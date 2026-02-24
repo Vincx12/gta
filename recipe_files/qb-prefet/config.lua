7c4e4151367108d382ef3e75ec627eff21806e0f
Vincx12 <dylanboullard@gmail.com>
2026-02-24 21:53:09 +0100

    Add files via upload

---
+++ recipe_files/qb-prefet/config.lua
@@ -0,0 +1,3 @@
+Config={}
+Config.PrefetJob='prefet'
+Config.DefaultTaxes={tva=0.20,carburant=0.10,services=0.05}
---
+++ recipe_files/qb-tribunal-basic/client/main.lua
@@ -0,0 +1,1 @@
+RegisterNetEvent('qb-tribunal-basic:client:notice', function(msg) TriggerEvent('chat:addMessage',{args={'^5[Tribunal]',msg}}) end)
