e0c295bf37cc40813617dc2d4e36a5903d6f66bd
Vincx12 <dylanboullard@gmail.com>
2026-02-24 23:48:37 +0100

    Delete recipe_files directory

--- recipe_files/qb-prefet/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
-shared_script 'config.lua'
-client_script 'client/main.lua'
-server_script 'server/main.lua'
--- recipe_files/qb_fuel_config.lua
+++
@@ -1,5 +0,0 @@
-Config = Config or {}
-Config.CostMultiplier = Config.CostMultiplier or 3.2
-Config.RefuelTime = Config.RefuelTime or 650
-Config.GlobalTax = Config.GlobalTax or 0.12
-Config.GasStations = Config.GasStations or {}
