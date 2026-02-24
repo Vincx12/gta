62f5fb8178e52723be65944e5732f8313d3a85ae
Vincx12 <dylanboullard@gmail.com>
2026-02-24 22:09:03 +0100

    Delete recipe_files directory

--- recipe_files/qb_vehicleshop_config.lua
+++
@@ -1,9 +0,0 @@
-Config = Config or {}
-Config.Locations = Config.Locations or {}
-Config.Commission = 0.10
-Config.FinanceCommission = 0.05
-Config.PaymentWarning = 10
-Config.PaymentInterval = 30
-Config.MinimumDown = 15
-Config.MaximumPayments = 36
-Config.Locations["shop"] = Config.Locations["shop"] or { coords = vector3(-56.9, -1096.9, 26.4) }
