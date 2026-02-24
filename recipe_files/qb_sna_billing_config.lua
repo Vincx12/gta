bc79c919df04baffb220c34c76df87e7ca90a46d
Vincx12 <dylanboullard@gmail.com>
2026-02-24 22:44:29 +0100

    Add files via upload

---
+++ recipe_files/qb_sna_billing_config.lua
@@ -0,0 +1,5 @@
+Config = Config or {}
+Config.BlacklistedJobs = { "unemployed" }
+Config.PayFrom = "bank"
+Config.CompanyAccount = "bank"
+Config.PoliceJobs = { "police" }
---
+++ recipe_files/qb_vehicleshop_config.lua
@@ -0,0 +1,8 @@
+Config = Config or {}
+Config.Locations = Config.Locations or {}
+Config.Commission = 0.10
+Config.FinanceCommission = 0.05
+Config.PaymentWarning = 10
+Config.PaymentInterval = 30
+Config.MinimumDown = 15
+Config.MaximumPayments = 48
