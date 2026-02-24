0d9191c1ad39ddd2707122a8db100dc1f3cd1e54
Vincx12 <dylanboullard@gmail.com>
2026-02-24 22:03:12 +0100

    Delete recipe_files directory

--- recipe_files/qb_fuel_config.lua
+++
@@ -1,5 +0,0 @@
-Config = Config or {}
-Config.CostMultiplier = Config.CostMultiplier or 3.0
-Config.RefuelTime = Config.RefuelTime or 650
-Config.GlobalTax = Config.GlobalTax or 0.08
-Config.GasStations = Config.GasStations or {}
